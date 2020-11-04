2347360750a4ecf21e1eedbe71e0a0052688535f
yiminprogram <minwork1016@outlook.com>
2020-11-05 03:44:01 +0800

    add table sort thead icon

--- src/components/Table/Index.js
+++ src/components/Table/Index.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+//material ui
+import {
+  ArrowUpward,
+  ArrowDownward,
+} from '@material-ui/icons';
 
 const Table = styled.table`
-  width: 500px;
+  min-width: 600px;
   box-shadow: 0 0 10px #ccc;
   border-collapse: collapse;
   border-radius: 10px;
   overflow: hidden;
 
   th {
+    cursor: pointer;
+    min-width: 100px;
     padding: 1rem;
     font-size: 1.3rem;
     font-weight: 700;
     color: #fff;
     background-color: #3f51b5;
+    vertical-align: middle;
+    white-space: nowrap;
   }
 
   td {
     font-size: 1.1rem;
     padding: 1rem;
     text-align: center;
+    vertical-align: middle;
   }
 
   tbody tr {
@@ -33,6 +43,17 @@ const Table = styled.table`
   tbody tr:last-of-type {
     border-bottom: none;
   }
+`;
+
+const SortIcon = styled(ArrowDownward)`
+  visibility: ${(p) =>
+    p.arr === undefined ? 'hidden' : 'visible'};
+  transform: ${(p) =>
+    p.arr !== undefined
+      ? p.arr.length === 1
+        ? 'rotate(0deg)'
+        : 'rotate(180deg)'
+      : 'none'};
 `;
 
 const Td = ({ data }) => {
@@ -49,13 +70,39 @@ const Tr = ({ data }) => {
   );
 };
 
+const Th = ({ data, state }) => {
+  return (
+    <th>
+      <SortIcon arr={state} />
+      <span>{data}</span>
+    </th>
+  );
+};
+
 const Thead = ({ head }) => {
+  const [state, setState] = useState([]);
+  const target = (e) => {
+    const tHead = e.target.textContent;
+    if (state.length === 2) {
+      setState([]);
+    } else if (state[0] !== tHead) {
+      setState([tHead]);
+    } else if (state.length === 0) {
+      setState([...state, tHead]);
+    } else if (state[0] === tHead) {
+      setState([...state, tHead]);
+    }
+  };
   return (
     <thead>
-      <tr>
-        {head.map((ele, idx) => (
-          <th key={idx}>{ele}</th>
-        ))}
+      <tr onClick={target}>
+        {head.map((ele, idx) =>
+          ele === state[0] ? (
+            <Th key={idx} data={ele} state={state} />
+          ) : (
+            <Th key={idx} data={ele} />
+          ),
+        )}
       </tr>
     </thead>
   );
